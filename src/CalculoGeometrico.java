4db9d0f54f35a2ddeffb07b4a15ef3d784d9bc73
André <andrecjuniorr@gmai.com>
2026-07-01 14:13:39 -0300

    Primeito teste

--- src/CalculoGeometrico.java
+++ src/CalculoGeometrico.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class CalculoGeometrico {
+/*"qual palavra-chave usamos quando o valor nunca pode ser alterado"
+é final, mas para criar uma constante de classe completa, combinamos
+com static.*/
+
+    public static final double PI = 3.14;
+
+    static double calcularAreaDoQuadrado(double ladoDoQuadrado) {
+        return ladoDoQuadrado * ladoDoQuadrado;
+    }
+/*Por que um metodo static acessa PI diretamente?
+Porque PI é static e está na mesma classe. Membros estáticos da mesma
+classe se enxergam diretamente — sem precisar de prefixo ou instância.
+Se PI estivesse em outra classe, aí precisaria do prefixo:
+CalculoGeometrico.PI.*/
+    static double calcularAreaDoCirculo(double raioDoCirculo ) {
+        return PI * raioDoCirculo * raioDoCirculo;
+    }
+
 }
+/*OBS foi para o metodo calcularAreaDoQuadrado exatamente virou o
+ladoDoQuadrado creio que agora eu entendi ele se transformou no valor do
+parametro como mostra na figura abaixo é isso ?
+Exatamente isso! O Java Visualizer mostrou perfeitamente:
+No main: lado = 4.0
+Dentro do metodo: ladoDoQuadrado = 4.0*/
--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Principal {
+/*Como seus métodos utilitários (como calcularAreaDoQuadrado e
+calcularAreaDoCirculo)e a constante PI foram declarados como static,
+ você NÃO precisa criar objetos (instâncias) da classe.Você pode chamar
+os métodos diretamente pelo nome da classe dentro do main*/
+    public static void main(String[] args) {
+        double lado = 4;
+        double raio = 5;
+
+        double areaDoQuadrado = CalculoGeometrico.calcularAreaDoQuadrado(lado);
+        double areaDoCirculo = CalculoGeometrico.calcularAreaDoCirculo(raio);
+
+        System.out.printf("A área do quadrado foi de: %.2f%n", areaDoQuadrado);
+        System.out.printf("A área do círculo foi de: %.2f%n", areaDoCirculo);
+    }
 }
